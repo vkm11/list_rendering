28732ccd2deaeffda7d966798ee7e3e846ee850e
vijaykumar <vijay1111mane@gmail.com>
2022-05-26 18:30:02 +0530

    commit;

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 v-for="(name, index) in names" :key="name">{{index}} {{name}}</h2>
+  <h2 v-for="name in fullNames" :key="name.first">{{name.first}} {{name.last}}</h2>
+
+<br>
+<div v-for="actor in actors" :key="actor.name">
+  <h2>{{actor.name}}</h2>
+  <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3>
+</div>
+
+<h2 v-for="(value, key, index) in myInfo" :key="value">{{index}} :- {{key}} :- {{value}}</h2>
+
+<template v-for="name in names" :key="name">
+<h2>{{name}}</h2>
+<hr />
+
+</template>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{ 
+      names:['vijay', 'gokul','prajwal','nithin'],
+      fullNames:[ 
+        { first: 'vijay', last: 'mane'},
+        { first: 'gokul', last: 'M K'},
+        { first: 'prajwal', last: 'something'},
+        { first: 'nithin', last: 'savkar'}
+      ],
+      actors:[ 
+        {
+          name: "1. Navrang theatre",
+          movies: ['Badava Rascal','Yuvarathnaa'],
+        },
+        {
+          name: "2. Balaji theatre",
+          movies: ['KGF part-2', 'Ek Love Ya']
+        },
+      ],
+      myInfo: {
+        name: 'Vijaykumar',
+        channel: 'Codevolution',
+        course: 'vue 3'
+      }
+    }
   }
 }
 </script>
